150ca8b918d21d22d626e359040bfb34d2da2670
Hieu Nguyen <minhhieun4@gmail.com>
2019-12-12 14:39:15 +0700

    add seed

--- main.py
+++ main.py
@@ -4,11 +4,25 @@ import sys
 import tensorflow as tf
 import numpy as np
 import yaml
+import random as rn
 from model.supervisor import EncoderDecoder
 
 config = tf.ConfigProto()
 session = tf.Session(config=config)
 
+def seed():
+    # The below is necessary for starting Numpy generated random numbers
+    # in a well-defined initial state.
+    np.random.seed(42)
+    # The below is necessary for starting core Python generated random numbers
+    # in a well-defined state.
+    rn.seed(12345)
+    # The below tf.set_random_seed() will make random number generation
+    # in the TensorFlow backend have a well-defined initial state.
+    # For further details, see:
+    # https://www.tensorflow.org/api_docs/python/tf/set_random_seed
+    tf.set_random_seed(1234)
+    
 
 def print_lstm_info(mode, config):
     print('----------------------- INFO -----------------------')
