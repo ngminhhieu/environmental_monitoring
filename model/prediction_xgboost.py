12342e803164875ed3fd4d7b89bc8f8f11d66816
Hieu Nguyen <minhhieun4@gmail.com>
2020-02-14 09:36:05 +0700

    add xgboost

--- model/prediction_xgboost.py
+++ model/prediction_xgboost.py
@@ -1,21 +1,29 @@
-import numpy as np; np.random.seed(0)
+import numpy as np;
 import pandas as pd
+import csv
+from datetime import datetime
 from numpy import sort
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
+import plotly_express as px
 
 def predict(dataset, cols_feature):
     # split data into X and y
     X = dataset.iloc[:,0:(len(cols_feature)-1)]
     Y = dataset.iloc[:,-1]
     # split data into train and test sets
-    train_size = int(len(dataset)*0.8)
+    train_size = int(len(dataset)*0.65)
+    valid_size = int(len(dataset)*0.15)
     X_train = X.iloc[0:train_size]
     y_train = Y.iloc[0:train_size]
-    
-    X_test = X.iloc[train_size:]
-    y_test = Y.iloc[train_size:]
 
+    X_valid = X.iloc[train_size:train_size+valid_size]
+    y_valid = Y.iloc[train_size:train_size+valid_size]
+    
+    X_test = X.iloc[train_size:train_size+valid_size:]
+    y_test = Y.iloc[train_size:train_size+valid_size:]
+    eval_set = [(X_valid, y_valid)]
+    
     # fit model on all training data
     model = XGBRegressor(
     max_depth=8,
@@ -25,29 +33,57 @@ def predict(dataset, cols_feature):
     subsample=0.8, 
     eta=0.3,    
     seed=42)
-
     model.fit(
     X_train, 
     y_train, 
     eval_metric="mae",
-    verbose=True, 
+    eval_set=eval_set,
+    verbose=False, 
     early_stopping_rounds = 10)
+
     # plot feature importance
-    # feature_importances = pd.DataFrame({'col': columns,'imp':model.feature_importances_})
-    # feature_importances = feature_importances.sort_values(by='imp',ascending=False)
-    # px.bar(feature_importances,x='col',y='imp')
+    feature_importances = pd.DataFrame({'col': cols_feature[0:-1],'imp':model.feature_importances_})
+    feature_importances = feature_importances.sort_values(by='imp',ascending=False)
+    px.bar(feature_importances, x='col',y='imp')
 
     # make predictions for test data and evaluate
     y_pred = model.predict(X_test)
     predictions = [round(value) for value in y_pred]
     mae = mean_absolute_error(y_test, predictions)
     print("MAE: %.3f" % (mae))
-    
+    return [mae]
+
+def write_log(input_features, error):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    error.insert(0, dt_string)
+    with open("log/xgboost/metrics.csv", 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(error)
+        writer.writerow(input_features)
 
 if __name__ == "__main__":
     # taiwan
     features = ['MONTH', 'DAY', 'YEAR', 'HOUR', 'AMB_TEMP', 'CO', 'NO', 'NO2',
-    'NOx', 'O3', 'RH', 'SO2', 'WD_HR', 'WIND_DIREC', 'WIND_SPEED', 'WS_HR', 'PM10', 'PM2.5']    
-    taiwan_dataset = pd.read_csv('data/csv/taiwan_data_mean.csv', usecols=features)
-    predict(taiwan_dataset, features)
+    'NOx', 'O3', 'RH', 'SO2', 'WD_HR', 'WIND_DIREC', 'WIND_SPEED', 'WS_HR', 'PM10']    
+    input_features = []
 
+    # random search
+    times_random_search = 20    
+    for time in range(1, 1+times_random_search):
+        # find input_features by random search
+        binary_features = np.random.randint(2, size=len(features))
+        for index, value in enumerate(binary_features, start=0):
+            if value == 1:
+                input_features.append(features[index])
+        print(binary_features)
+        input_features.append('PM2.5')
+        taiwan_dataset = pd.read_csv('data/csv/taiwan_data_mean.csv', usecols=input_features)
+        error_mae = predict(taiwan_dataset, input_features)
+
+        # reset input_features
+        write_log(input_features, error_mae)        
+        input_features = []
+        
+    
+        
