84a3492f0f67f860cad9a5f497b797e38b662711
Hieu Nguyen <minhhieun4@gmail.com>
2020-02-20 14:39:44 +0700

    fix

--- lib/GA.py
+++ lib/GA.py
@@ -33,7 +33,7 @@ def get_input_features(gen_array):
     return input_features
 
 def get_dataset(input_features):
-    path = 'data/csv/taiwan_test_short.csv'
+    path = 'data/csv/taiwan_data_mean.csv'
     taiwan_dataset = pd.read_csv(path, usecols=input_features+[target_feature])
     new_dataset = utils.data_preprocessing(taiwan_dataset, input_features, target_feature)
     X_train, y_train, X_valid, y_valid, X_test, y_test = utils.split_data(new_dataset, 0.65, 0.15)
--- lib/test.py
+++ lib/test.py
@@ -1,5 +1,7 @@
 import GA
 import utils
 import constant
-evo = GA.evolution(nb_feature=8, total_feature=len(constant.features), pc=0.2, pm=0.2, population_size=20, max_gen=30)
-utils.write_log(path="log/GA/", filename="result.csv", error=evo)
+
+for i in range(len(constant.features)):
+    evo = GA.evolution(nb_feature=i, total_feature=len(constant.features), pc=0.2, pm=0.2, population_size=50, max_gen=200)
+    utils.write_log(path="log/GA/", filename="result.csv", error=evo)
