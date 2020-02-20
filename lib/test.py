ad5124e1f84968eba99b585589b4f4a250e7b5c5
Hieu Nguyen <minhhieun4@gmail.com>
2020-02-20 21:35:05 +0700

    fix

--- lib/test.py
+++ lib/test.py
@@ -2,6 +2,6 @@ import GA
 import utils
 import constant
 
-for i in range(len(constant.features)):
-    evo = GA.evolution(nb_feature=i, total_feature=len(constant.features), pc=0.2, pm=0.2, population_size=20, max_gen=200)
+for i in range(1, len(constant.features)+1):
+    evo = GA.evolution(nb_feature=i, total_feature=len(constant.features), pc=0.2, pm=0.2, population_size=50, max_gen=200)
     utils.write_log(path="log/GA/", filename="result.csv", error=evo)
