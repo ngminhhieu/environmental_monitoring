a5263ea0584c129c9f8f7e5f507896b77f42296a
Hieu Nguyen <minhhieun4@gmail.com>
2020-02-24 11:22:04 +0700

    fix

--- lib/test_ga.py
+++ lib/test_ga.py
@@ -3,6 +3,7 @@ import utils
 import constant
 
 for i in range(5, len(constant.features)):
+    print(i)
     evo = GA.evolution(nb_feature=i, total_feature=len(constant.features), pc=0.2, pm=0.2, population_size=50, max_gen=100)
     fitness = [i, evo[0]["gen"], evo[0]["fitness"]]
     utils.write_log(path="log/GA/", filename="result.csv", error=fitness)
