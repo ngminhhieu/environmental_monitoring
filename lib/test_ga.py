73ddc092bcdb4000e4bc01a55373b1c8b4ccd523
Hieu Nguyen <minhhieun4@gmail.com>
2020-02-26 14:23:29 +0700

    fix

--- lib/test_ga.py
+++ lib/test_ga.py
@@ -3,6 +3,6 @@ import utils
 import constant
 import GABinary
 
-evo=GABinary.evolution(total_feature=len(constant.features), pc=0.2, pm=0.2, population_size=50, max_gen=1010)
+evo=GABinary.evolution(total_feature=len(constant.features), pc=0.2, pm=0.2, population_size=50, max_gen=400)
 fitness = [evo["gen"], evo["fitness"]]
 utils.write_log(path="log/GA/", filename="result_binary.csv", error=fitness)
