a6c59f00253b83d9667f9c742853ca382f75938b
Hieu Nguyen <minhhieun4@gmail.com>
2020-04-01 23:58:23 +0700

    fix

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.mode == 'ga':
-        evo = evolution(total_feature=len(constant.hanoi_features), pc=0.2, pm=0.2, population_size=1, max_gen=3)
+        evo = evolution(total_feature=len(constant.hanoi_features), pc=0.2, pm=0.2, population_size=2, max_gen=3)
         fitness = [evo["gen"], evo["fitness"]]
         utils_ga.write_log(path="log/GA/", filename="result_binary.csv", error=fitness)
     else:
