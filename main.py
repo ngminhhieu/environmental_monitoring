3ca21b237711cb90f0505ca3d861f21843eea060
Hieu Nguyen <minhhieun4@gmail.com>
2021-02-07 10:31:06 +0700

    fix

--- main.py
+++ main.py
@@ -7,10 +7,28 @@ import yaml
 import random as rn
 from model.supervisor import EncoderDecoder
 
+# allow run multiple command python (sharing GPU)
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+
+def checkGPU():
+    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        # Restrict TensorFlow to only use the first GPU
+        try:
+            tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        except RuntimeError as e:
+            # Visible devices must be set before GPUs have been initialized
+            print(e)
+
 def seed():
     # The below is necessary for starting Numpy generated random numbers
     # in a well-defined initial state.
-    np.random.seed(42)
+    np.random.seed(2)
     # The below is necessary for starting core Python generated random numbers
     # in a well-defined state.
     rn.seed(12345)
@@ -18,7 +36,7 @@ def seed():
     # in the TensorFlow backend have a well-defined initial state.
     # For further details, see:
     # https://www.tensorflow.org/api_docs/python/tf/set_random_seed
-    tf.set_random_seed(1234)
+    tf.random.set_seed(1234)
     
 
 def print_lstm_info(mode, config):
@@ -62,27 +80,24 @@ def print_lstm_info(mode, config):
 
 
 def train_lstm_ed(config):
-    with tf.device('/device:GPU:{}'.format(config['gpu'])):
-        model = EncoderDecoder(is_training=True, **config)
-        model.plot_models()
-        model.train()
+    model = EncoderDecoder(is_training=True, **config)
+    model.plot_models()
+    model.train()
 
 
 def test_lstm_ed(config):
-    with tf.device('/device:GPU:{}'.format(config['gpu'])):
-        model = EncoderDecoder(is_training=False, **config)
-        model.test()
-        model.plot_series()
+    model = EncoderDecoder(is_training=False, **config)
+    model.test()
+    model.plot_series()
 
 
 def evaluate_lstm_ed(config):
-    with tf.device('/device:GPU:{}'.format(config['gpu'])):
-        model = EncoderDecoder(is_training=False, **config)
-        model.evaluate()
+    model = EncoderDecoder(is_training=False, **config)
+    model.evaluate()
 
 
 if __name__ == '__main__':
-    np.random.seed(1)
+    seed()
     sys.path.append(os.getcwd())
     parser = argparse.ArgumentParser()
     parser.add_argument('--use_cpu_only', default=False, type=str, help='Whether to run tensorflow on cpu.')
